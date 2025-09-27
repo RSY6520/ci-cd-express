3c641fafbc686d993cc763c43b1f0182036ead3b
Ramakant Yadav <ext.ramakant@zeliot.in>
2025-09-27 20:43:23 +0530

    added random route

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get("/", (req, res) => {
   res.json({ message: "CI/CD demo working 🚀" });
 });
 
+app.get("/randomNumber", (req, res) => {
+  res.json({ message: Math.random() });
+});
+
 module.exports = app;
