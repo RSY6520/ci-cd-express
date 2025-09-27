801dfa4d748401ce185acdb21df5ecbb7c4c1050
Ramakant Yadav <ext.ramakant@zeliot.in>
2025-09-27 17:57:27 +0530

    testing negative case

--- index.test.js
+++ index.test.js
@@ -4,7 +4,7 @@ const app = require("./index");
 describe("GET /", () => {
   it("should return success message", async () => {
     const res = await request(app).get("/");
-    expect(res.statusCode).toBe(200);
+    expect(res.statusCode).toBe(209);
     expect(res.body.message).toBe("CI/CD demo working 🚀");
   });
 });
